a3c6eff33383982564cf5bace99456c44fb01125
Karanbhatia110 <karanbhatia9780@gmail.com>
2026-07-22 13:50:22 +0530

    ADDED STREAMING IN frontend

--- chatbot with ui/frontend.py
+++ chatbot with ui/frontend.py
@@ -20,10 +20,18 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = workflow.invoke({'messages': HumanMessage(content = user_input)}, config = CONFIG)
+    # response = workflow.invoke({'messages': HumanMessage(content = user_input)}, config = CONFIG)
 
-    ai_message = response['messages'][-1].content
+    # ai_message = response['messages'][-1].content
 
-    st.session_state['message_history'].append({'role':'assistant' , 'content' : ai_message})
     with st.chat_message('assistant'):
-        st.text(ai_message) 
+
+        ai_message = st.write_stream(
+            message_chunk.text for message_chunk, metadata in workflow.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode= 'messages'
+            )
+        )
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
